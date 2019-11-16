bff6fa1e859d33dfee70b6f4bd154e52f27c544a
Varun Master <varunmaster95@gmail.com>
2019-11-16 17:35:36 -0500

    added scrape route and connected to mongo

--- server.js
+++ server.js
@@ -16,3 +16,32 @@ app.use(express.static("public")); //this allows us to serve up the index.html p
 
 
 mongoose.connect("mongodb://localhost/ice-scraper", { useNewUrlParser: true });
+
+app.post("/scrape", (req, res) => {
+    axios.get("https://old.reddit.com/r/frugalmalefashion/").then((page) => {
+        var $ = cheerio.load(page.data); //all the html (tags included) get saved in here
+        //console.log($);
+        $("p.title").each(function(i, element) { //must use word function, arrow function is no bueno --> THAT IS SO DUMB SPENT LIKE 20 MINS DEBUGGING BC OF THIS
+            //console.log(element); //this is interesting, should take a look at this later
+            var result = {};
+
+            result.title = $(this).text();
+
+            result.link = $(this).children("a").attr("href");
+            
+            // console.log(result);
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                console.log(dbArticle);
+            })
+            .catch(function(err) {
+                return res.json(err);
+            });
+        });
+        res.redirect("/");
+    });
+});
+
+app.listen(3000, function() {
+    console.log("App running on port 3000" + "!");
+});
